a76b675e6bd8a3b375c186da50c002f8979f396d
maggie.wan <maggie.wan@aiunion.com.tw>
2019-07-12 13:17:15 +0800

    add:android unit test for deleting and updating

--- app/src/androidTest/java/com/example/student/DAOandSQLite/MyDAOTest1.java
+++ app/src/androidTest/java/com/example/student/DAOandSQLite/MyDAOTest1.java
@@ -25,23 +25,59 @@ public class MyDAOTest1 {
         phone p1 = dao.getOne(1);
         assertEquals("BBB",p1.name);
     }
-//    @Test
-//    public void clearAndAddOneDataAndGetTest()
-//    {
-//        Context appContext = InstrumentationRegistry.getTargetContext();
-//        phoneDAODBImpl dao = new phoneDAODBImpl(appContext);
-//        phone p = new phone();
-//        p.name = "BBB";
-//        p.tel = "123";
-//        p.addr = "aabb";
-//        //清除資料後再新增一筆測試，確保資料庫正確
-//        dao.clearAll();
-//        dao.addOne(p);
-//        //取得資料庫的資料放到陣列中
-//        phone pArray[] = dao.getList();
-//        //取得第一筆資料中的name來測試
-//        assertEquals("BBB", pArray[0].name);
-//
-//    }
+    @Test
+    public void clearAndAddOneDataAndGetTest()
+    {
+        Context appContext = InstrumentationRegistry.getTargetContext();
+        phoneDAODBImpl dao = new phoneDAODBImpl(appContext);
+        phone p = new phone();
+        p.name = "BBB";
+        p.tel = "123";
+        p.addr = "aabb";
+        //清除資料後再新增一筆測試，確保資料庫正確
+        dao.clearAll();
+        dao.addOne(p);
+        //取得資料庫的資料放到陣列中
+        phone pArray[] = dao.getList();
+        //取得第一筆資料中的name來測試
+        assertEquals("BBB", pArray[0].name);
+
+    }
+
+
+    //測試刪除
+    @Test
+    public void testDelete1(){
+        Context appContext = InstrumentationRegistry.getTargetContext();
+        phoneDAODBImpl dao = new phoneDAODBImpl(appContext);
+        phone p1=new phone("ccc","333","123456");
+        phone p2=new phone("ddd","444","123456");
+        dao.clearAll();
+        dao.addOne(p1);
+        dao.addOne(p2);
+        phone pArray[]=dao.getList();
+        //因為前面new c1時沒有給id，所以這邊設定id給c1
+        p1.id=pArray[0].id;
+        dao.delete(p1);
+        phone pArray2[]=dao.getList();
+        assertEquals("ddd",pArray2[0].name);
+    }
+
+    //測試更新
+    @Test
+    public void testUpdate(){
+        Context appContext = InstrumentationRegistry.getTargetContext();
+        phoneDAODBImpl dao = new phoneDAODBImpl(appContext);
+        phone p1=new phone("ccc","333","123456");
+
+        dao.clearAll();
+        dao.addOne(p1);
+        phone parray[]=dao.getList();
+        p1.id=parray[0].id;
+        p1.name="fff";
+        dao.update(p1);
+        phone parray2[]=dao.getList();
+        assertEquals("fff", parray2[0].name);
+    }
 }
 
